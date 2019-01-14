2df062447867915ca2d2eb8e048f89d8e50e7917
zhao1009 <zhao1009@umn.edu>
2019-01-14 22:22:47 +0800

    remove Trash folder from default targets

--- lib/targets.sh
+++ lib/targets.sh
@@ -1,8 +1,7 @@
 targets() {
-  local TRASH_FOLDER="$HOME/.Trash/*"
   local USER_CACHE="$HOME/Library/Caches/*"
   local USER_LOGS="$HOME/Library/Logs/*"
   local APP_CACHE="$HOME/Library/Containers/*/Data/Library/Caches/*"
 
-  echo "$TRASH_FOLDER:$USER_CACHE:$USER_LOGS:$APP_CACHE"
+  echo "$USER_CACHE:$USER_LOGS:$APP_CACHE"
 }
